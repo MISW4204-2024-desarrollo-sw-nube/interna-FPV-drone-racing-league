bad65a2ee0b434757f1fcfec51061ebc7e1d265a
ccpinzonh1 <cc.pinzonh1@uniandes.edu.co>
2024-04-07 21:01:49 -0500

    issue #1: Creacion de Endpoint para cargar videos y procesar su edicion

---
+++ uploader/api_commands.py
@@ -0,0 +1,49 @@
+import os
+
+from flask import request
+from moviepy.editor import VideoFileClip, concatenate_videoclips, ImageClip
+from werkzeug.utils import secure_filename
+
+from base import app
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file part'
+    file = request.files['file'
+    ]
+    filename = secure_filename(file.filename)
+
+    # Create directory if not exists
+    os.makedirs(app.config['UNPROCESSED_FOLDER'], exist_ok=True)
+    os.makedirs(app.config['PROCESSED_FOLDER'], exist_ok=True)
+
+    file.save(os.path.join(app.config['UNPROCESSED_FOLDER'], filename))
+
+    # TODO: process the video in a separate function, We should use a task queue like Celery for this. this function is blocking and will not scale well
+
+    # Process the video
+    clip = VideoFileClip(os.path.join(app.config['UNPROCESSED_FOLDER'], filename))
+
+    # Shorten the video if it's longer than 20 seconds
+    if clip.duration > 20:
+        clip = clip.subclip(0, 20)
+
+    # Add images at the beginning and end of the video
+    img_clip = ImageClip(os.path.join(os.getcwd(), 'res', 'maxresdefault.jpg'), duration=1)
+    final_clip = concatenate_videoclips([img_clip, clip, img_clip])
+
+    # Change the aspect ratio to 16:9
+    final_clip = final_clip.resize(height=720)  # Resize height to 720p
+    final_clip = final_clip.crop(x_center=final_clip.w / 2, y_center=final_clip.h / 2, width=1280,
+                                 height=720)  # Crop to 16:9
+
+    # Save the final video
+    final_clip.write_videofile(os.path.join(app.config['PROCESSED_FOLDER'], 'final_' + filename))
+
+    return 'File uploaded and processed successfully'
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
