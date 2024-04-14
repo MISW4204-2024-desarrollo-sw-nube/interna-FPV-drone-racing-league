368509f974715ad3f3881590b5ee2161b44a0995
Juan Carlos De Jesus <j.dejesus@uniandes.edu.co>
2024-04-14 00:35:30 -0500

    Implementa doble verificacion de password

--- authentication/api_commands.py
+++ authentication/api_commands.py
@@ -4,10 +4,13 @@ from flask_jwt_extended import create_access_token
 
 @app.route('/api/users/signup', methods=['POST'])
 def signup_user():
-   if "useremail" not in request.json or "username" not in request.json or "userpassword" not in request.json:
-        return "Email, username or password are invalid. Please try again.", 400
+   if "useremail" not in request.json or "username" not in request.json or "password1" not in request.json or "password2" not in request.json:
+        return "useremail, username, password1 or password2 are invalid. Please try again.", 400
 
-   user = Usuario(useremail=request.json["useremail"], username=request.json["username"], userpassword=request.json["userpassword"])
+   if request.json["password1"] != request.json["password2"]:
+      return "password1 and password2 do not match. Please try again."
+
+   user = Usuario(useremail=request.json["useremail"], username=request.json["username"], userpassword=request.json["password1"])
    db.session.add(user)
    db.session.commit()
    
@@ -16,11 +19,11 @@ def signup_user():
 
 @app.route('/api/users/login', methods=['POST'])
 def login_user():
-   if "username" not in request.json or "userpassword" not in request.json:
-        return "Email, username of password are invalid. Please try again.", 400
+   if "username" not in request.json or "password" not in request.json:
+        return "username or password are invalid. Please try again.", 400
 
    usuario = Usuario.query.filter(Usuario.username == request.json["username"],
-                                       Usuario.userpassword == request.json["userpassword"]).first()
+                                       Usuario.userpassword == request.json["password"]).first()
    if usuario is None:
       return "User does not exist. Please try again.", 404
 
