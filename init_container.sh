97a6789e630cb3ed7e5b4f37e9a7bc3b46a22641
Andrés Felipe Losada luna <nedrocoli@gmail.com>
2024-04-24 22:20:14 -0500

    feature: script for template

---
+++ init_container.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Obtenido de https://tomroth.dev/gcp-docker/
+sudo apt update 
+sudo apt install --yes apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install --yes docker-ce
+sudo apt-get install docker-compose-plugin
+sudo apt-get install git
+
+# Clonando el repo
+git clone https://github.com/MISW4204-2024-desarrollo-sw-nube/interna-FPV-drone-racing-league.git
