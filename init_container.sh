5ff15a8a0a718ebbfb5e9dd4130ced493f39af9d
Juan De Jesus <j.dejesus@uniandes.edu.co>
2024-05-04 11:40:45 -0500

    Modifica init_container

--- init_container.sh
+++ init_container.sh
@@ -4,6 +4,12 @@ SERVICE=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attri
 
 echo $SERVICE
 
+# Instalación de agentes logs para monitoreo
+sudo apt-get update
+curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+sudo bash add-google-cloud-ops-agent-repo.sh --also-install
+sudo systemctl status google-cloud-ops-agent"*"
+
 sudo apt update 
 sudo apt install --yes apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
@@ -19,10 +25,3 @@ git clone https://github.com/MISW4204-2024-desarrollo-sw-nube/interna-FPV-drone-
 cd interna-FPV-drone-racing-league
 
 docker compose up $SERVICE
-
-# Instalación de agentes logs para monitoreo
-if $SERVICE == "uploader"
-then
-    curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
-    sudo bash add-google-cloud-ops-agent-repo.sh --also-install
-    sudo systemctl status google-cloud-ops-agent"*"
