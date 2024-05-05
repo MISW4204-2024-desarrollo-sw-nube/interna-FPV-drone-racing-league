99de4c9525817493d2d11cbb12f6f96e08818aed
Andrés Felipe Losada luna <af.losada@uniandes.edu.co>
2024-05-05 09:53:07 -0500

    removing $ from string

--- uploader/celeryconfig.py
+++ uploader/celeryconfig.py
@@ -3,4 +3,4 @@ import os
 worker_send_task_events = True
 task_send_sent_event = True
 brokerURL = os.environ.get('BROKER_URL', 'broker')
-broker_url = f'redis://${brokerURL}:6379/0'
+broker_url = f'redis://{brokerURL}:6379/0'
