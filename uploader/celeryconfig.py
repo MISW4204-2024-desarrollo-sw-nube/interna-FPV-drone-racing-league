e4ed30779b0f52fcb6e2f8cb73018f6b415dde2d
Andrés Felipe Losada luna <af.losada@uniandes.edu.co>
2024-05-05 09:22:47 -0500

    fixing string

--- uploader/celeryconfig.py
+++ uploader/celeryconfig.py
@@ -1,7 +1,6 @@
 import os
 
-
 worker_send_task_events = True
 task_send_sent_event = True
 brokerURL = os.environ['BROKER_URL', 'broker']
-broker_url = 'redis://${brokerURL}:6379/0'
+broker_url = f'redis://${brokerURL}:6379/0'
